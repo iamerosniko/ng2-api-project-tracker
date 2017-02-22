aa7d2d64d93db577dd04b608820597154f0bb361
Eros Niko Alvarez <iamerosniko@yahoo.com>
2017-02-22 14:33:32 -0500

    up

---
+++ solution/ng2-api/Controllers/DetailsController.cs
@@ -0,0 +1,67 @@
+﻿using ng2_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace ng2_api.Controllers
+{
+    public class DetailsController : ApiController
+    {
+        public PTContext db = new PTContext();
+        //on load display all project's details
+        public List<PT_ProjectDetails_DTO> Get(System.Guid ID)
+        {
+            List<PT_ProjectDetails_DTO> details = new List<PT_ProjectDetails_DTO>();
+            //IQueryable<PT_Projects> tempProjects = db.PT_Projects;
+            IQueryable<PT_ProjectDetails> tempDetails = from l in db.PT_ProjectDetails
+                                                        where l.pt_detail_show == true
+                                                        where l.pt_detail_deleted == false
+                                                        where l.pt_project_id == ID
+                                                        select l;
+
+            foreach (PT_ProjectDetails detail in tempDetails)
+            {
+                details.Add(new PT_ProjectDetails_DTO
+                {
+                    pt_detail_actend = detail.pt_detail_actend,
+                    pt_detail_actstart = detail.pt_detail_actstart,
+                    pt_detail_assignee = detail.pt_detail_assignee,
+                    pt_detail_deleted = detail.pt_detail_deleted,
+                    pt_detail_deliverable = detail.pt_detail_deliverable,
+                    pt_detail_description = detail.pt_detail_description,
+                    pt_detail_estend = detail.pt_detail_estend,
+                    pt_detail_eststart = detail.pt_detail_eststart,
+                    pt_detail_id = detail.pt_detail_id,
+                    pt_detail_priority = detail.pt_detail_priority,
+                    pt_detail_show = detail.pt_detail_show,
+                    pt_detail_status = detail.pt_detail_status,
+                    pt_detail_task = detail.pt_detail_task,
+                    pt_project_id = detail.pt_project_id
+
+                });
+            }
+            return details;
+        }
+
+        // POST api/projects
+        [ResponseType(typeof(PT_Projects))]
+        public void Post(PT_Projects project)
+        {
+            db.Entry(project).State = EntityState.Added;
+            db.SaveChanges();
+        }
+
+        // PUT api/projects/5
+        [ResponseType(typeof(PT_Projects))]
+        public void Put(PT_Projects project)
+        {
+            db.Entry(project).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+    }
+}
---
+++ solution/ng2-api/Models/PT_ProjectDetails_DTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ng2_api.Models
+{
+    public class PT_ProjectDetails_DTO
+    {
+        public System.Guid pt_detail_id { get; set; }
+        public System.Guid pt_project_id { get; set; }
+        public string pt_detail_priority { get; set; }
+        public string pt_detail_task { get; set; }
+        public string pt_detail_assignee { get; set; }
+        public string pt_detail_description { get; set; }
+        public string pt_detail_deliverable { get; set; }
+        public Nullable<System.DateTime> pt_detail_eststart { get; set; }
+        public Nullable<System.DateTime> pt_detail_estend { get; set; }
+        public Nullable<System.DateTime> pt_detail_actstart { get; set; }
+        public Nullable<System.DateTime> pt_detail_actend { get; set; }
+        public string pt_detail_status { get; set; }
+        public bool pt_detail_deleted { get; set; }
+        public bool pt_detail_show { get; set; }
+    }
+}
