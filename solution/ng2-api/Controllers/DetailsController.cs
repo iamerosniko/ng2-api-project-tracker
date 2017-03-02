53d447c09d516db5aace2f1fdb20220e3e48b23c
Eros Niko Alvarez <iamerosniko@yahoo.com>
2017-03-02 16:41:56 -0500

    up

--- solution/ng2-api/Controllers/DetailsController.cs
+++ solution/ng2-api/Controllers/DetailsController.cs
@@ -25,33 +25,7 @@ namespace ng2_api.Controllers
                                                         where l.pt_project_id == projectID
                                                         select l;
 
-            foreach (PT_ProjectDetails detail in tempDetails)
-            {
-                details.Add(new PT_ProjectDetails_DTO
-                {
-                    pt_detail_actend = getValue(detail.pt_detail_actend),
-                    pt_detail_actstart = getValue(detail.pt_detail_actstart),
-                    pt_detail_assignee = detail.pt_detail_assignee,
-                    pt_detail_deleted = detail.pt_detail_deleted,
-                    pt_detail_deliverable = detail.pt_detail_deliverable,
-                    pt_detail_description = detail.pt_detail_description,
-                    pt_detail_estend = getValue( detail.pt_detail_estend),
-                    pt_detail_eststart =  getValue(detail.pt_detail_eststart),
-                    pt_detail_id = detail.pt_detail_id,
-                    pt_detail_priority = detail.pt_detail_priority,
-                    pt_detail_show = detail.pt_detail_show,
-                    pt_detail_status = detail.pt_detail_status,
-                    pt_detail_task = detail.pt_detail_task,
-                    pt_project_id = detail.pt_project_id,
-                    pt_detail_onhold= detail.pt_detail_onhold,
-                    pt_detail_reason=detail.pt_detail_reason,
-                    pt_detail_progress=detail.pt_detail_progress,
-                    pt_detail_entrytype=detail.pt_detail_entrytype
-                    
-
-                });
-            }
-            return details;
+            return fetchData(tempDetails);
         }
 
         //gets selected detail.
@@ -84,6 +58,23 @@ namespace ng2_api.Controllers
             };
         }
 
+        [Route("api/details/GetTaskItems")]
+        public List<PT_ProjectDetails_DTO> GetTaskItems(System.Guid projectID)
+        {
+            List<PT_ProjectDetails_DTO> details = new List<PT_ProjectDetails_DTO>();
+            //IQueryable<PT_Projects> tempProjects = db.PT_Projects;
+            IQueryable<PT_ProjectDetails> tempDetails = from l in db.PT_ProjectDetails
+                                                        where l.pt_detail_onhold == false
+                                                        where l.pt_detail_show == true
+                                                        where l.pt_detail_deleted == false
+                                                        where l.pt_detail_entrytype == "Task"
+                                                        where l.pt_project_id == projectID
+                                                        select l;
+
+
+            return fetchData(tempDetails);
+        }
+
         [Route("api/details/GetCompletedItems")]
         public List<PT_ProjectDetails_DTO> GetCompletedItems(System.Guid projectID)
         {
@@ -96,33 +87,8 @@ namespace ng2_api.Controllers
                                                         where l.pt_detail_status == "Completed"
                                                         select l;
 
-            foreach (PT_ProjectDetails detail in tempDetails)
-            {
-                details.Add(new PT_ProjectDetails_DTO
-                {
-                    pt_detail_actend = getValue(detail.pt_detail_actend),
-                    pt_detail_actstart = getValue(detail.pt_detail_actstart),
-                    pt_detail_assignee = detail.pt_detail_assignee,
-                    pt_detail_deleted = detail.pt_detail_deleted,
-                    pt_detail_deliverable = detail.pt_detail_deliverable,
-                    pt_detail_description = detail.pt_detail_description,
-                    pt_detail_estend = getValue(detail.pt_detail_estend),
-                    pt_detail_eststart = getValue(detail.pt_detail_eststart),
-                    pt_detail_id = detail.pt_detail_id,
-                    pt_detail_priority = detail.pt_detail_priority,
-                    pt_detail_show = detail.pt_detail_show,
-                    pt_detail_status = detail.pt_detail_status,
-                    pt_detail_task = detail.pt_detail_task,
-                    pt_project_id = detail.pt_project_id,
-                    pt_detail_onhold = detail.pt_detail_onhold,
-                    pt_detail_reason = detail.pt_detail_reason,
-                    pt_detail_progress = detail.pt_detail_progress,
-                    pt_detail_entrytype = detail.pt_detail_entrytype
 
-
-                });
-            }
-            return details;
+            return fetchData(tempDetails);
         }
 
         [Route("api/details/GetOnholdItems")]
@@ -137,42 +103,8 @@ namespace ng2_api.Controllers
                                                         where l.pt_detail_onhold == true
                                                         select l;
 
-            foreach (PT_ProjectDetails detail in tempDetails)
-            {
-                details.Add(new PT_ProjectDetails_DTO
-                {
-                    pt_detail_actend = getValue(detail.pt_detail_actend),
-                    pt_detail_actstart = getValue(detail.pt_detail_actstart),
-                    pt_detail_assignee = detail.pt_detail_assignee,
-                    pt_detail_deleted = detail.pt_detail_deleted,
-                    pt_detail_deliverable = detail.pt_detail_deliverable,
-                    pt_detail_description = detail.pt_detail_description,
-                    pt_detail_estend = getValue(detail.pt_detail_estend),
-                    pt_detail_eststart = getValue(detail.pt_detail_eststart),
-                    pt_detail_id = detail.pt_detail_id,
-                    pt_detail_priority = detail.pt_detail_priority,
-                    pt_detail_show = detail.pt_detail_show,
-                    pt_detail_status = detail.pt_detail_status,
-                    pt_detail_task = detail.pt_detail_task,
-                    pt_project_id = detail.pt_project_id,
-                    pt_detail_onhold = detail.pt_detail_onhold,
-                    pt_detail_reason = detail.pt_detail_reason,
-                    pt_detail_progress = detail.pt_detail_progress,
-                    pt_detail_entrytype = detail.pt_detail_entrytype
-
-
-                });
-            }
-            return details;
+            return fetchData(tempDetails);
         }
-
-        private string getValue(Nullable<System.DateTime> dt)
-        {
-            if (dt == null)
-                return "";
-            return String.Format("{0:yyyy-MM-dd}", dt); 
-        }
-
         // POST api/projects
         [ResponseType(typeof(PT_ProjectDetails))]
         public void Post(PT_ProjectDetails detail)
@@ -188,5 +120,47 @@ namespace ng2_api.Controllers
             db.Entry(detail).State = EntityState.Modified;
             db.SaveChanges();
         }
+
+
+        // EXtra Functions
+        private List<PT_ProjectDetails_DTO> fetchData(IQueryable<PT_ProjectDetails> tempDetails)
+        {
+            List<PT_ProjectDetails_DTO> details = new List<PT_ProjectDetails_DTO>();
+
+            foreach (PT_ProjectDetails detail in tempDetails)
+            {
+                details.Add(new PT_ProjectDetails_DTO
+                {
+                    pt_detail_actend = getValue(detail.pt_detail_actend),
+                    pt_detail_actstart = getValue(detail.pt_detail_actstart),
+                    pt_detail_assignee = detail.pt_detail_assignee,
+                    pt_detail_deleted = detail.pt_detail_deleted,
+                    pt_detail_deliverable = detail.pt_detail_deliverable,
+                    pt_detail_description = detail.pt_detail_description,
+                    pt_detail_estend = getValue(detail.pt_detail_estend),
+                    pt_detail_eststart = getValue(detail.pt_detail_eststart),
+                    pt_detail_id = detail.pt_detail_id,
+                    pt_detail_priority = detail.pt_detail_priority,
+                    pt_detail_show = detail.pt_detail_show,
+                    pt_detail_status = detail.pt_detail_status,
+                    pt_detail_task = detail.pt_detail_task,
+                    pt_project_id = detail.pt_project_id,
+                    pt_detail_onhold = detail.pt_detail_onhold,
+                    pt_detail_reason = detail.pt_detail_reason,
+                    pt_detail_progress = detail.pt_detail_progress,
+                    pt_detail_entrytype = detail.pt_detail_entrytype
+                });
+            }
+
+            return details;
+        }
+        private string getValue(Nullable<System.DateTime> dt)
+        {
+            if (dt == null)
+                return "";
+            return String.Format("{0:yyyy-MM-dd}", dt); 
+        }
+
+       
     }
 }
